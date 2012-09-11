f389fe3ac5dfbe269bdb913d47ddee17b9b46fb4
Emile Victor <emilevictor@gmail.com>
2012-09-12 01:25:09 +1000

    Changed the seeds.rb to create a ton of fake users and teams, along with fake tags.

--- QR-ScanTrak/db/seeds.rb
+++ QR-ScanTrak/db/seeds.rb
@@ -5,13 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'bcrypt'
 
+Team.delete_all
+Tag.delete_all
+User.delete_all
 
 for i in 0..2000
 	@newTag = Tag.new
 	@newTag.name = RandomWord.adjs.next + ' ' + RandomWord.nouns.next
-	@newTag.uniqueUrl = (0...10).map{ ('a'..'z').to_a[rand(26)] }.join
-	@newTag.content = (RandomWord.adjs.next + ' ' + RandomWord.nouns.next)*32
+	@newTag.uniqueUrl = (0...20).map{ ('a'..'z').to_a[rand(26)] }.join
 	@newTag.latitude = -150+rand(300)
 	@newTag.longitude = -150+rand(300)
 	@newTag.createdBy = 1
@@ -19,3 +22,29 @@ for i in 0..2000
 	@newTag.user_id = 1
 	@newTag.save
 end
+
+emile = User.create!(:email => "emilevictor@gmail.com", :password => "password", :password_confirmation => "password", :first_name => "Emile", :last_name => "Victor", :admin => true)
+
+emile.save
+
+for i in 0..500
+
+	user = User.create!(:email => "#{RandomWord.adjs.next}#{RandomWord.adjs.next}@gmail.com",:password => 'ohhai124124', :password_confirmation => 'ohhai124124', :first_name => "#{RandomWord.nouns.next.gsub("_"," ")}", :last_name => "#{RandomWord.nouns.next.gsub("_"," ")}")
+	if user.save
+		puts "new user created"
+	else
+		puts user.errors
+	end
+end
+
+
+for i in 0..100
+	@team1 = Team.new(:name => "The #{RandomWord.adjs.next.gsub("_"," ")} #{RandomWord.nouns.next.gsub("_"," ").pluralize}".capitalize, :password=>BCrypt::Password.create("HI"), :description => "hello")
+	@team1.users << User.find(User.first.id+rand(400))
+	@team1.users << User.find(User.first.id+rand(400))
+	@team1.users << User.find(User.first.id+rand(400))
+	@team1.users << User.find(User.first.id+rand(400))
+	@team1.save
+end
+
+
