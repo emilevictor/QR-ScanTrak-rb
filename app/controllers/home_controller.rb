818426c949416d7b8e748b0340079c75a6ef5631
Emile Victor <emilevictor@gmail.com>
2012-09-12 21:26:39 +1000

    Attempt to make it work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,14 @@ class HomeController < ApplicationController
   def index
   	if user_signed_in?
 		@user = current_user
-		@team = Team.find(@user.team_id)
+		if not @user.nil?
+
+			@team = Team.find(@user.team_id)
+
+		else
+			flash[:alert] = "There didn't seem to be a user signed in"
+
+		end
   	end
   end
 end
