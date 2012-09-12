56194b040b837bf1078594c36fe4d50d7fbf7afe
Emile Victor <emilevictor@gmail.com>
2012-09-12 21:57:30 +1000

    Added admin column

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
 
   
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :admin
+  attr_accessible :email, :password, :admin, :password_confirmation,
+   :remember_me, :first_name, :last_name
   # attr_accessible :title, :body
 end
